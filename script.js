689023d595c84797c559c20701166adbfce3f42f
joaovctr <joaosteam26@gmail.com>
2023-10-16 11:46:07 -0400

    verificação da altura

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function calcular() {
   let peso = document.getElementById("peso").value;
   let altura = document.getElementById("altura").value;
   let resultado = document.getElementById("resultado");
-  
-  let calculo = peso / ((altura * altura) / 10000);
+  let calculo;
   resultado.style.cssText = `
             background-color: #8ae287;
             padding: 10px;
@@ -11,10 +10,19 @@ function calcular() {
             max-width: 300px;
             `;
 
+//VALIDAÇÂO SE O PESO É ME CENTIMETROS OU METROS
   if (peso == "" && altura == "") {
     resultado.innerHTML = ``;
     resultado.style.cssText = `background-color: #fff;`
-  } else if (calculo < 18.5) {
+  } else if (altura.length == 3) {
+  calculo = peso / ((altura * altura) / 10000);
+  }
+  else if (altura.length == 4) {
+  calculo = peso / ((altura * altura));
+}
+
+//COMPARAÇÃO DO VALOR COM A TABELA
+if (calculo < 18.5) {
     resultado.innerHTML = `O seu imc é de ${calculo.toFixed(1)} Kg/m2. Seu peso esta abaixo do ideal, procure um profissional da área.`;
   } else if (calculo > 18.4 && calculo < 24.9) {
     resultado.innerHTML = `O seu imc é de ${calculo.toFixed(1)} Kg/m2. E seu peso está normal.`;
@@ -27,4 +35,5 @@ function calcular() {
   } else if (calculo > 39.9) {
     resultado.innerHTML = `O seu imc é de ${calculo.toFixed(1)} Kg/m2. E você esta com Obesidade grau III, procure um profissional da área.`;
   }
+
 }
